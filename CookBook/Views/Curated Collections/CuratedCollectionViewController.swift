3afc070efdd474fd5744253476ea2b5625b8c7f6
Paras <prgorasiya@yahoo.com>
2022-01-26 16:13:30 +0530

    add Curated Collection view controller class and linked with its respective storyboard

---
+++ CookBook/Views/Curated Collections/CuratedCollectionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CuratedCollectionViewController.swift
+//  CookBook
+//
+//  Created by paras gorasiya on 26/01/22.
+//
+
+import UIKit
+
+class CuratedCollectionViewController: UIViewController {
+    @IBOutlet weak var mainCollectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
