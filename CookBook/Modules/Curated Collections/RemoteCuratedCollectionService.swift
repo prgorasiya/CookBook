4b015f130da596b0392d13245eb9352c3d82b0a8
Paras <prgorasiya@yahoo.com>
2022-01-26 15:02:14 +0530

    add Implementation of CuratedCollectionService to load collections from API

---
+++ CookBook/Modules/Curated Collections/RemoteCuratedCollectionService.swift
@@ -0,0 +1,40 @@
+//
+//  RemoteCuratedCollectionService.swift
+//  CookBook
+//
+//  Created by paras gorasiya on 26/01/22.
+//
+
+import Foundation
+
+public final class RemoteCuratedCollectionService: CuratedCollectionService {
+    private let url: URL
+    private let client: APIClient
+
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+
+    public init(url: URL, client: APIClient) {
+        self.url = url
+        self.client = client
+    }
+
+    public func load(completion: @escaping (CuratedCollectionService.Result) -> Void) {
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+
+            switch result {
+            case let .success((data, response)):
+                guard response.statusCode == 200, let images = try? RemoteCuratedCollectionMapper.curatedCollection(from: data) else {
+                    completion(.failure(Error.invalidData))
+                    return
+                }
+                completion(.success(images))
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        }
+    }
+}
