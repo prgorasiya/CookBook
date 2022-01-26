add283cc4aa850d2f4e54d0c6664bbf05d4e2226
Paras <prgorasiya@yahoo.com>
2022-01-26 22:05:15 +0530

    add recipe collection cell

---
+++ CookBook/Views/Curated Collections/Cell/CuratedCollectionCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CuratedCollectionCollectionViewCell.swift
+//  CookBook
+//
+//  Created by paras gorasiya on 26/01/22.
+//
+
+import Foundation
+import UIKit
+
+class CuratedCollectionCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var recipeCountLabel: UILabel!
+
+    func updateCell(with model: CuratedCollection) {
+        
+    }
+}
