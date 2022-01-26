be19fb9088e47fb9af16d8232b9c50a55252240a
Paras <prgorasiya@yahoo.com>
2022-01-26 16:01:46 +0530

    add APIClient implementation for calling GET APIs

---
+++ CookBook/API Client/APIManager.swift
@@ -0,0 +1,31 @@
+//
+//  APIManager.swift
+//  CookBook
+//
+//  Created by paras gorasiya on 26/01/22.
+//
+
+import Foundation
+
+private class APIManager: APIClient {
+    func get(from url: URL, completion: @escaping (APIClient.Result) -> Void) {
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: urlRequest) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            if let data = data, let httpResponse = response as? HTTPURLResponse {
+                completion(.success((data, httpResponse)))
+            }
+            else {
+                let unknownError = NSError(domain: "Unknown Error", code: 0)
+                completion(.failure(unknownError))
+            }
+        }
+        .resume()
+    }
+}
