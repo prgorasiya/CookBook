e792fe265d7d0032af21e9ab1b61b77f4efe44b3
Paras <prgorasiya@yahoo.com>
2022-01-27 16:44:26 +0530

    add TableView and CollectionView extensions for registering and dequeuing cells

---
+++ CookBook/Extensions/CollectionView+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  CollectionView+Extension.swift
+//  CookBook
+//
+//  Created by paras gorasiya on 27/01/22.
+//
+
+import Foundation
+import UIKit
+
+extension UICollectionViewCell: ReusableView, NibLoadableView {}
+
+extension UICollectionView {
+    func register<T: UICollectionViewCell>(_: T.Type) {
+        let bundle = Bundle(for: T.self)
+        let nib = UINib(nibName: T.nibName, bundle: bundle)
+        register(nib, forCellWithReuseIdentifier: T.defaultReuseIdentifier)
+    }
+
+    func dequeueReusableCell<T: UICollectionViewCell>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.defaultReuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
+        }
+        return cell
+    }
+}
---
+++ CookBook/Extensions/TableView+Extension.swift
@@ -0,0 +1,47 @@
+//
+//  TableView+Extension.swift
+//  CookBook
+//
+//  Created by paras gorasiya on 27/01/22.
+//
+
+import Foundation
+import UIKit
+
+protocol ReusableView: AnyObject {
+    static var defaultReuseIdentifier: String { get }
+}
+
+extension ReusableView where Self: UIView {
+    static var defaultReuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableViewCell: ReusableView, NibLoadableView {}
+
+protocol NibLoadableView: AnyObject {
+    static var nibName: String { get }
+}
+
+extension NibLoadableView where Self: UIView {
+    static var nibName: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableView {
+    func register<T: UITableViewCell>(_: T.Type) {
+        let bundle = Bundle(for: T.self)
+        let nib = UINib(nibName: T.nibName, bundle: bundle)
+        register(nib, forCellReuseIdentifier: T.defaultReuseIdentifier)
+    }
+
+
+    func dequeueReusableCell<T: UITableViewCell>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: T.defaultReuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
+        }
+        return cell
+    }
+}
