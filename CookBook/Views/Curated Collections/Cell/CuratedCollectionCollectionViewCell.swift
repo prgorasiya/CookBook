2fed6d3958073041a94cbca26541dc828a915fe7
Paras <prgorasiya@yahoo.com>
2022-01-27 17:42:37 +0530

    add code to set property values from model

--- CookBook/Views/Curated Collections/Cell/CuratedCollectionCollectionViewCell.swift
+++ CookBook/Views/Curated Collections/Cell/CuratedCollectionCollectionViewCell.swift
@@ -9,11 +9,21 @@ import Foundation
 import UIKit
 
 class CuratedCollectionCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var recipeCountLabel: UILabel!
 
     func updateCell(with model: CuratedCollection) {
-        
+        //previewImageUrls is not supposed to be empty as per API docs, but one object has empty previewImageUrls, therefore this check
+        if let imageURL = model.previewImageUrls.first {
+            imageView.setImageWith(urlString: imageURL, placeholderImage: nil, showActivityIndicator: true)
+        }
+        containerView.layer.cornerRadius = 10
+        titleLabel.text = model.title
+        titleLabel.layer.cornerRadius = 4
+        recipeCountLabel.text = "\(model.recipeCount)"
+        recipeCountLabel.layer.cornerRadius = recipeCountLabel.frame.size.height/2
+        layoutIfNeeded()
     }
 }
