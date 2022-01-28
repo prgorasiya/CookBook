231ba6b003e980e2fae40ee0eb82a776f0e330b4
Paras <prgorasiya@yahoo.com>
2022-01-28 16:16:23 +0530

    delivers invalid data error with Invalid json delivers success with valid json

--- CookBookTests/LoadCuratedCollectionFromRemoteUseCaseTests.swift
+++ CookBookTests/LoadCuratedCollectionFromRemoteUseCaseTests.swift
@@ -47,6 +47,67 @@ class LoadCuratedCollectionFromRemoteUseCaseTests: XCTestCase {
         }
     }
 
+    func test_load_deliversInvalidDataErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+
+        expect(sut, toCompleteWith: .failure(.invalidData), when: {
+            let invalidJSON = Data("invalid json".utf8)
+            client.complete(withStatusCode: 200, data: invalidJSON)
+        })
+    }
+
+    func test_load_deliversInvalidDataErrorOn200HTTPResponseWithPartiallyValidJSONItems() {
+        let (sut, client) = makeSUT()
+
+        let validItem = makeItem(
+            id: anyRandomWholeNumber(),
+            recipeCount: anyRandomWholeNumber(),
+            previewImageUrls: ["http://another-url.com"]
+        ).json
+
+        let invalidItem = [["invalid": "item"]]
+
+        let items = validItem + invalidItem
+
+        expect(sut, toCompleteWith: .failure(.invalidData), when: {
+            let json = makeItemsJSON(items)
+            client.complete(withStatusCode: 200, data: json)
+        })
+    }
+
+    func test_load_deliversSuccessWithNoItemsOn200HTTPResponseWithEmptyJSONList() {
+        let (sut, client) = makeSUT()
+
+        expect(sut, toCompleteWith: .success([]), when: {
+            let emptyListJSON = makeItemsJSON([])
+            client.complete(withStatusCode: 200, data: emptyListJSON)
+        })
+    }
+
+    func test_load_deliversSuccessWithItemsOn200HTTPResponseWithJSONItems() {
+        let (sut, client) = makeSUT()
+
+        let item1 = makeItem(
+            id: anyRandomWholeNumber(),
+            recipeCount: anyRandomWholeNumber(),
+            previewImageUrls: ["http://a-url.com"])
+
+        let item2 = makeItem(
+            id: anyRandomWholeNumber(),
+            title: "a title",
+            description: "a description",
+            recipeCount: anyRandomWholeNumber(),
+            previewImageUrls: ["http://another-url.com"])
+
+        let items = [item1.model, item2.model]
+
+        expect(sut, toCompleteWith: .success(items), when: {
+            let array = item1.json + item2.json
+            let json = makeItemsJSON(array)
+            client.complete(withStatusCode: 200, data: json)
+        })
+    }
+
     // MARK: - Helpers
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteCuratedCollectionService, client: APIClientSpy) {
         let client = APIClientSpy()
@@ -54,7 +115,31 @@ class LoadCuratedCollectionFromRemoteUseCaseTests: XCTestCase {
         return (sut, client)
     }
 
+    private func makeItem(id: Int,
+                          title: String = "Any title",
+                          description: String = "Any description",
+                          recipeCount: Int,
+                          previewImageUrls: [String]) -> (model: CuratedCollection, json: [[String: Any]]) {
+        let item = CuratedCollection(id: id, title: title, description: description, recipeCount: recipeCount, previewImageUrls: previewImageUrls)
+
+        let object = [
+            "id": id,
+            "title": title,
+            "description": description,
+            "recipe_count": recipeCount,
+            "preview_image_urls": previewImageUrls
+        ].compactMapValues { $0 }
+
+        let json = [object]
+
+        return (item, json)
+    }
+
     private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
         return try! JSONSerialization.data(withJSONObject: items)
+    }
+
+    private func anyRandomWholeNumber() -> Int {
+        Int.random(in: 1...1000)
     }
 }
