b2f0519ba94fcb65f39bebab4fb1668fa703843a
Paras <prgorasiya@yahoo.com>
2022-01-28 15:08:21 +0530

    extract helper method to create Client and SUT instances

--- CookBookTests/LoadCuratedCollectionFromRemoteUseCaseTests.swift
+++ CookBookTests/LoadCuratedCollectionFromRemoteUseCaseTests.swift
@@ -10,19 +10,25 @@ import XCTest
 class LoadCuratedCollectionFromRemoteUseCaseTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = APIClientSpy()
+        let (_, client) = makeSUT()
 
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
 
     func test_loadTwice_requestsDataFromURLTwice() {
         let url = URL(string: "https://a-given-url.com")!
-        let client = APIClientSpy()
-        let sut = RemoteCuratedCollectionService(url: url, client: client)
+        let (sut, client) = makeSUT()
 
         sut.load { _ in }
         sut.load { _ in }
 
         XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+
+    // MARK: - Helpers
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteCuratedCollectionService, client: APIClientSpy) {
+        let client = APIClientSpy()
+        let sut = RemoteCuratedCollectionService(url: url, client: client)
+        return (sut, client)
     }
 }
