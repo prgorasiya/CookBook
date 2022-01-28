e055279eba0d94e9a1caa53f8bcd7d9500ab9797
Paras <prgorasiya@yahoo.com>
2022-01-28 11:43:50 +0530

    add some skeleton methods for RecipeDetails view

--- CookBook/Views/Recipe Details/RecipeDetailViewController.swift
+++ CookBook/Views/Recipe Details/RecipeDetailViewController.swift
@@ -8,6 +8,38 @@
 import Foundation
 import UIKit
 
+typealias RecipeDetailDataSource = UITableViewDiffableDataSource<String?, AnyHashable>
+typealias RecipeDetailSnapshot = NSDiffableDataSourceSnapshot<String?, AnyHashable>
+
 class RecipeDetailViewController: UIViewController {
-    
+    @IBOutlet weak var tableView: UITableView!
+
+    var recipe: Recipe!
+    private var viewModel: RecipeDetailViewModel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = recipe.title
+
+        viewModel = RecipeDetailViewModel()
+        setupUI()
+        bindViewModel()
+        viewModel.createDataSource()
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+        print("deinit RecipeDetailViewController screen......")
+    }
+
+    func setupUI() {
+        navigationController?.navigationBar.isHidden = false
+        tableView.tableFooterView = UIView()
+    }
+
+    func bindViewModel() {
+        viewModel.dataSource = UITableViewDiffableDataSource(tableView: tableView, cellProvider: { (tableView, indexPath, model) -> UITableViewCell? in
+            return nil
+        })
+    }
 }
--- CookBook/Views/Recipe Details/RecipeDetailViewModel.swift
+++ CookBook/Views/Recipe Details/RecipeDetailViewModel.swift
@@ -8,5 +8,17 @@
 import Foundation
 
 class RecipeDetailViewModel {
-    
+    var dataSource: RecipeDetailDataSource!
+    var snapshot = RecipeDetailSnapshot()
+
+    func createDataSource() {
+
+    }
+
+    func updateDataSource(_ recipes: [AnyHashable]) {
+        snapshot.deleteAllItems()
+        snapshot.appendSections([""])
+        snapshot.appendItems(recipes)
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
 }
