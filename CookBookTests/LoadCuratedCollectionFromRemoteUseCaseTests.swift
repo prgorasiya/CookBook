f8e02584db09589a1d7477904933a580cd6c3601
Paras <prgorasiya@yahoo.com>
2022-01-28 15:39:34 +0530

    test delivers Connectivity error on client error and Invalid Data error on Non 200 HTTP Response

--- CookBookTests/LoadCuratedCollectionFromRemoteUseCaseTests.swift
+++ CookBookTests/LoadCuratedCollectionFromRemoteUseCaseTests.swift
@@ -25,10 +25,60 @@ class LoadCuratedCollectionFromRemoteUseCaseTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
 
+    func test_load_deliversConnectivityErrorOnClientError() {
+        let (sut, client) = makeSUT()
+
+        let clientError = NSError(domain: "Test", code: 0)
+
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case .failure(let receivedError):
+                XCTAssertEqual(receivedError as! RemoteCuratedCollectionService.Error, RemoteCuratedCollectionService.Error.connectivity)
+
+            default:
+                XCTFail("Expected result \(clientError)")
+            }
+            exp.fulfill()
+        }
+
+        client.complete(with: clientError)
+
+        waitForExpectations(timeout: 0.1)
+    }
+
+    func test_load_deliversInvalidDataErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+
+        let samples = [199, 201, 300, 400, 500]
+        let json = makeItemsJSON([])
+
+        samples.enumerated().forEach { index, code in
+            let exp = expectation(description: "Wait for load completion")
+            
+            sut.load { result in
+                switch result {
+                case .failure(let receivedError):
+                    XCTAssertEqual(receivedError as! RemoteCuratedCollectionService.Error, RemoteCuratedCollectionService.Error.invalidData)
+
+                default:
+                    XCTFail("Expected result \(RemoteCuratedCollectionService.Error.invalidData)")
+                }
+                exp.fulfill()
+            }
+            client.complete(withStatusCode: code, data: json, at: index)
+            waitForExpectations(timeout: 0.1)
+        }
+    }
+
     // MARK: - Helpers
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteCuratedCollectionService, client: APIClientSpy) {
         let client = APIClientSpy()
         let sut = RemoteCuratedCollectionService(url: url, client: client)
         return (sut, client)
+    }
+
+    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+        return try! JSONSerialization.data(withJSONObject: items)
     }
 }
