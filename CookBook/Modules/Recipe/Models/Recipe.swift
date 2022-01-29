a8d2c93275f02d7b319d50053e0bf9864d294da5
Paras <prgorasiya@yahoo.com>
2022-01-29 10:22:42 +0530

    add back hashable value which is used to distinguish between 2 instances

--- CookBook/Modules/Recipe/Models/Recipe.swift
+++ CookBook/Modules/Recipe/Models/Recipe.swift
@@ -15,14 +15,38 @@ public struct Recipe: Hashable {
     public let user: User
     public let ingredients: [String]
     public let steps: [Step]
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+
+    public static func == (lhs: Recipe, rhs: Recipe) -> Bool {
+        return lhs.id == rhs.id
+    }
 }
 
 public struct User: Hashable {
     public let name: String
     public let imageUrl: String?
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
+
+    public static func == (lhs: User, rhs: User) -> Bool {
+        return lhs.name == rhs.name
+    }
 }
 
 public struct Step: Hashable {
     public let description: String
     public let imageUrls: [String]
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(description)
+    }
+
+    public static func == (lhs: Step, rhs: Step) -> Bool {
+        return lhs.description == rhs.description
+    }
 }
